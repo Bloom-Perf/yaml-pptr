4b32155c9e9256ad6191642af7a2a26c19a24f3b
Alexandre Careil <alex.careil.fr@gmail.com>
2023-10-20 16:22:49 +0200

    Enrich parsing test

--- test/yaml/parser.spec.ts
+++ test/yaml/parser.spec.ts
@@ -11,9 +11,14 @@ scenarios:
         - action: NAVIGATE
           url: "http://example.com/page1"
     - name: Scenario 2
-      steps: []
-        `)
+      steps: []`);
+
         expect(test.scenarios).to.be.instanceOf(Array).and.lengthOf(2);
+        expect(test.scenarios[0].name).to.be.equal("Scenario 1");
+        expect(test.scenarios[0].steps[0].action).to.be.equal("NAVIGATE");
+        expect(test.scenarios[0].steps[0].url).to.be.equal("http://example.com/page1");
+        expect(test.scenarios[1].name).to.be.equal("Scenario 2");
+        expect(test.scenarios[1].steps).to.be.instanceOf(Array).and.lengthOf(0);
 
     });
 });
